38fc223a1ee8acec08f9ac4a0a8b66840eb988a5
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-02-10 16:41:43 +0100

    create Search component and add a form input using useState hook

---
+++ src/components/Search.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const Search = () => {
+  const [term, setTerm] = useState("");
+console.log(term)
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={e => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
