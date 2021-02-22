d5eb3a0fe22399cd4857bc04913e044c2d355dea
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-02-22 11:41:34 +0000

    create a async searchWikipedia function and use axios to make an API call to wikepedia

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState("");
-console.log(term)
+  const [term, setTerm] = useState('programming');
+  const [result, setResults] = useState([]);
+  
+  useEffect (() => {
+    const searchWikipedia = async () => {
+      const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    searchWikipedia();
+  },[term])
+
   return (
     <div>
       <div className="ui form">
