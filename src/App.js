7373503ae212e255520cd2b35403c6461d143608
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-02-23 15:20:20 +0000

    create a options array object, storing a list of objects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
   {
@@ -20,11 +21,27 @@ const items = [
   }
 ];
 
+const options = [
+  {
+    label: 'The Colour Red',
+    value: 'red'
+  },
+  {
+    label: 'The Colour Green',
+    value: 'green'
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue'
+  }
+];
+
 export default () => {
   return (
     <div>
-      {/* <Accordion items={items} /> */}
+      <Accordion items={items} />
       <Search />
+      <Dropdown options={options}/>
     </div>
   );
 };
