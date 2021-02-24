16122f7264118334eefe634cd189dd18dfc768f1
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-02-24 10:52:05 +0000

    create a Translate component, and add scaffolding

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import Accordion from "./components/Accordion";
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 const items = [
   {
@@ -43,11 +44,12 @@ export default () => {
     <div>
       {/* <Accordion items={items} />
       <Search /> */}
-      <Dropdown 
+      {/* <Dropdown 
         selected={selected}
         onSelectedChange={setSelected} 
         options={options}
-      />
+      /> */}
+      <Translate />
     </div>
   );
 };
