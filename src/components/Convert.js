495f2ff62fdcd8d3d5383a14917b1fdb4426c8da
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-02-24 12:17:07 +0000

    create a Convert component, add scaffolding

---
+++ src/components/Convert.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect} from 'react';
+
+const Convert = ({ language, text }) => {
+
+  return(
+    <div>
+      Convert
+    </div>
+  )
+};
+
+export default Convert;
