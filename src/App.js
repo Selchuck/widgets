3795b31fe6ccd9d748a8d8214115eb29ef5e77ed
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-03-01 13:59:29 +0000

    create a Route file, build a basic reusable Route Component and show it in the App Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from "./components/Accordion";
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
 
 const items = [
   {
@@ -42,15 +43,23 @@ export default () => {
 
   return (
     <div>
-      {/* <Accordion items={items} />
-      <Search /> */}
-      {/* <Dropdown
-        label="Select a Colour" 
-        selected={selected}
-        onSelectedChange={setSelected} 
-        options={options}
-      /> */}
-      <Translate />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown 
+          label="Select a Colour" 
+          selected={selected}
+          onSelectedChange={setSelected} 
+          options={options}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
